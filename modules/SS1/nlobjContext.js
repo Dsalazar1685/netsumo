d28ce29da3bcf42c17519821e7610e1623d377d9
Doug Salazar <doug.salazar@fleetmatics.com>
2018-03-21 13:42:47 -0400

    Add get- and set- execution context methods

--- modules/SS1/nlobjContext.js
+++ modules/SS1/nlobjContext.js
@@ -3,11 +3,21 @@ var clone = require('clone')
 module.exports = function () {
 
   var name = '';
+  var executionContext = '';
   var user = undefined;
   var usage = 1000; //needs to be 10000 for scheduled scripts
 
   var getName = function() {
     return name;
+  }
+
+  var getExecutionContext = function() {
+    return executionContext;
+  }
+
+  //netsumo only function for manually creating a script execution context
+  var setExecutionContext = function(newContext) {
+    executionContext = newContext;
   }
 
   var getUser = function() {
@@ -52,6 +62,8 @@ module.exports = function () {
     getUser : getUser,
     setName : setName,
     setUser : setUser,
+    getExecutionContext : getExecutionContext,
+    setExecutionContext : setExecutionContext,
     getScriptId : getScriptId,
     getDeploymentId : getDeploymentId,
     getRemainingUsage : getRemainingUsage,
