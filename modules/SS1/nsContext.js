4db777d8a7b5fbee70a464b3d081081608b547f6
Doug Salazar <doug.salazar@fleetmatics.com>
2018-04-05 09:28:50 -0400

    Fix nlapiSubmitField reference errors

--- modules/SS1/nsContext.js
+++ modules/SS1/nsContext.js
@@ -312,7 +312,7 @@ exports.getDefaultContext = function(opts) {
     nlobjContext.decreaseUnits(10);
     for (var i = 0 ; i < recordsArray.length ; i ++) {
       var record = recordsArray[i];
-      if (record.getType() === type && record.getId() === id) {
+      if (record.getRecordType() === type && record.getId() === id) {
         if (Array.isArray(fields) && Array.isArray(values)) {
           fields.forEach(function(field, index) {
             record.setFieldValue(field, values[index]);
@@ -434,6 +434,7 @@ var nlapiGetOldRecord = function() {
     getScriptStatus: getScriptStatus,
     nlapiGetContext: nlapiGetContext,
     nlapiGetOldRecord : nlapiGetOldRecord,
+    nlapiSubmitField : nlapiSubmitField,
   };
 
 };
